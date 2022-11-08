a3009927ef4b6fda2787d83157f4b7730713507f
a01620485 <104392762+a01620485@users.noreply.github.com>
2022-11-08 12:40:09 -0600

    AÑADIR CLASES DE CATEGORÍA

---
+++ app/src/main/java/diego/estrada/deersign/category.kt
@@ -0,0 +1,10 @@
+package diego.estrada.deersign
+
+import android.graphics.Color
+import android.graphics.drawable.Icon
+
+data class category (
+    val nombre: String,
+    val icono: Int,
+    val color: String
+        )
---
+++ app/src/main/java/diego/estrada/deersign/word.kt
@@ -0,0 +1,7 @@
+package diego.estrada.deersign
+
+data class word (
+    val id: String,
+    val nombre: String,
+    val source_path: String,
+    val categoria: String)
