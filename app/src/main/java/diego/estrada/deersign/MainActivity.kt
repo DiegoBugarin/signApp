ff6d60fe209db30d93b052e4d7bd75ecc3abfb67
a01620485 <104392762+a01620485@users.noreply.github.com>
2022-11-08 23:42:35 -0600

    Añadir pantalla de perfil y rutas de navegación en nav_graph

--- app/src/main/java/diego/estrada/deersign/MainActivity.kt
+++ app/src/main/java/diego/estrada/deersign/MainActivity.kt
@@ -2,14 +2,27 @@ package diego.estrada.deersign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import diego.estrada.deersign.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    //lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(binding.root)
 
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.btmNavView)
+
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        /*val bottomNavView = binding.btmNavView
+        val navController = findNavController(R.id.fragmentContainerView)*/
+
+        bottomNavView.setupWithNavController(navController)
     }
 }
